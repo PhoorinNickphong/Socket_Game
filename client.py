48fdf95f1164a4d4623ff45e50257b872ecfb3da
Synai Sunthorn <sinaisunthorn@gmail.com>
2023-03-03 15:16:52 +0700

    recieve data and start game from server

--- client.py
+++ client.py
@@ -7,3 +7,10 @@ PORT = 65432        # The port used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     print('Connected to server')    
+
+    # รับข้อมูลจาก server ว่าเริ่มเกมหรือยัง
+    data = s.recv(1024)
+    print(data.decode())
+    
+    while True:
+        guess = input('ลองทายเลขสิ 1 ถึง 100 หนะ!: ')
